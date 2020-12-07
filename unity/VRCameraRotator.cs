964f3072fc461a25f8475d8d0d73786a66e02545
Made Indrayana <indra@doubleshot-audio.com>
2020-12-07 15:16:21 +0100

    VRCameraRotator.cs, no movement possible yet WIP

---
+++ unity/VRCameraRotator.cs
@@ -0,0 +1,110 @@
+﻿/*
+        ▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷
+        ◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁
+                                                                                           
+             ((((                                                                          
+            ((((     _______       _______.     ___                                        
+             ))))   |       \     /       |    /   \                                       
+          _ .---.   |  .--.  |   |   (----`   /  ^  \                                      
+         ( |`---'|  |  |  |  |    \   \      /  /_\  \    _                                
+          \|     |  |  '--'  |.----)   |    /  _____  \  /   _   _|  _        _  ._ |   _  
+          : .___, : |_______/ |_______/    /__/     \__\ \_ (_) (_| (/_ \/\/ (_) |  |< _>  
+           `-----'                                                                         
+                                                                                           
+        ▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷▷
+        ◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁◁
+*/
+
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class DSAPlayerController : MonoBehaviour
+{
+    // DSA - Enable Movement with WASD
+    public bool isMovementAllowed = false;
+
+    /// Camera.
+    public Camera mainCamera;
+
+    // Character controller.
+    private CharacterController characterController = null;
+
+    // Player movement speed.
+    private float movementSpeed = 5.0f;
+
+    // Target camera rotation in degrees.
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
+    // Maximum allowed vertical rotation angle in degrees.
+    private const float clampAngleDegrees = 80.0f;
+
+    // Camera rotation sensitivity.
+    private const float sensitivity = 2.0f;
+
+    void Start()
+    {
+#if UNITY_STANDALONE && !UNITY_EDITOR
+        SetCursorLock(true);
+#endif // Set cursor lock on build
+
+        characterController = GetComponent<CharacterController>();
+        Vector3 rotation = mainCamera.transform.localRotation.eulerAngles;
+        rotationX = rotation.x;
+        rotationY = rotation.y;
+    }
+
+    void LateUpdate()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetCursorLock(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SetCursorLock(false);
+        }
+#endif  // UNITY_EDITOR
+
+
+        // Update the rotation.
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            // Note that multi-touch control is not supported on mobile devices.
+            mouseX = 0.0f;
+            mouseY = 0.0f;
+        }
+        rotationX += sensitivity * mouseY;
+        rotationY += sensitivity * mouseX;
+        rotationX = Mathf.Clamp(rotationX, -clampAngleDegrees, clampAngleDegrees);
+        mainCamera.transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0.0f);
+        // Update the position.
+        if (isMovementAllowed)
+        {
+            float movementX = Input.GetAxis("Horizontal");
+            float movementY = Input.GetAxis("Vertical");
+            Vector3 movementDirection = new Vector3(movementX, 0.0f, movementY);
+            movementDirection = mainCamera.transform.localRotation * movementDirection;
+            movementDirection.y = 0.0f;
+            characterController.SimpleMove(movementSpeed * movementDirection);
+        }
+    }
+
+    // Sets the cursor lock for first-person control.
+    private void SetCursorLock(bool lockCursor)
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
